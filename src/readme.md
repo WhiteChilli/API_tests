5f6355c6e53aec3df663f811e47d9dfdb975c1cf
WhiteChilli <locmerea@gmail.com>
2023-03-14 08:21:52 +0200

    Homework 10 updated

---
+++ src/readme.md
@@ -0,0 +1,14 @@
+| Nr  |                              API Tests                               | Status |
+|:----|:--------------------------------------------------------------------:|:------:|
+| 1   |           Positive test - correct order ID and status code           |   OK   |
+| 2   |          Negative test - incorrect order ID and status code          |   OK   |
+| 3   |  Negative test with parameters - incorrect order ID and status code  |   OK   |
+| 4   |   Positive test with parameters - correct order ID and status code   |   OK   |
+| 5   |             Negative test - post request without header              |   OK   |
+| 6   |      Positive test check status code was "OPEN" with correct ID      |   OK   |
+
+| Nr  |                          PetStore API Tests                          | Status |
+|:----| :------------------------------------------------------------------: |:------:|
+| 1   |          Correct IDs - check order placed message returned           |   OK   |
+| 2   |                   Correct IDs - check status code                    |   OK   |
+| 3   |                  Incorrect IDs - check status code                   |   OK   |
--- src/test/java/org/example/PetStoreAPI.java
+++ src/test/java/org/example/PetStoreAPI.java
@@ -1,4 +1,66 @@
 package org.example;
 
+import io.restassured.RestAssured;
+import org.apache.http.HttpStatus;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static io.restassured.RestAssured.given;
+
 public class PetStoreAPI {
+
+    @BeforeEach
+    public void setup() {
+        System.out.println("---> test start");
+        RestAssured.baseURI = "http://51.250.6.164";
+        RestAssured.port = 8080;
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {3, 5})
+    public void correctIDsCheckOrderPlaced(int id) {
+        String responseString = given().
+                log().
+                all().
+                when().
+                get("/store/order/{id}", id).
+                then().
+                log().
+                all().
+                statusCode(HttpStatus.SC_OK).
+                and().
+                extract().
+                asString();
+        Assertions.assertTrue(responseString.contains("placed"));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 10})
+    public void correctIDsParamTest(int orderId) {
+
+        given().
+                log().
+                all().
+                when().
+                get("/store/order/{orderId}", orderId).
+                then().
+                statusCode(HttpStatus.SC_OK);
+
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {11, 0})
+    public void wrongIDsParamTest(int orderId) {
+
+        given().
+                log().
+                all().
+                when().
+                get("/store/order/{orderId}", orderId).
+                then().
+                statusCode(HttpStatus.SC_BAD_REQUEST);
+
+    }
 }
