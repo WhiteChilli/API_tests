445cf2bf91ad0bfff21b045e781fb3017e645718
WhiteChilli <locmerea@gmail.com>
2023-03-26 16:46:05 +0300

    API tests - notes from lesson

--- src/test/java/delivery/DeliveryTest.java
+++ src/test/java/delivery/DeliveryTest.java
@@ -5,6 +5,8 @@ import dto.OrderDto;
 import dto.OrderRealDto;
 import helpers.SetupFunctions;
 import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -91,4 +93,125 @@ public class DeliveryTest {
                 .extract()
                 .response();
     }
+
+    @Test
+    public void getOrderById() {
+        int id = orderCreationPrecondition();
+
+        int receivedId = given()
+                .header("Content-type", "application/json")
+                .header("Authorization", "Bearer " + token)
+                .log()
+                .all()
+                .get("/orders" + "/" + id)
+                .then()
+                .log()
+                .all()
+                .assertThat()
+                .statusCode(200)
+                .extract()
+                .path("id");
+
+        Assertions.assertEquals(receivedId, id);
+    }
+
+
+    @Test
+    public void getOrderByNonExistentId() {
+        int id = 2796;
+
+        String response = given()
+                .header("Content-type", "application/json")
+                .header("Authorization", "Bearer " + token)
+                .log()
+                .all()
+                .get("/orders" + "/" + id)
+                .then()
+                .log()
+                .all()
+                .assertThat()
+                .statusCode(200)
+                .extract()
+                .response()
+                .asString();
+
+        Assertions.assertEquals("",response);
+    }
+
+    @Test
+    public void getOrders(){
+
+        int id = orderCreationPrecondition();
+
+        OrderRealDto[] orderRealDtoArray = given()
+                .header("Content-type", "application/json")
+                .header("Authorization", "Bearer " + token)
+                .log()
+                .all()
+                .get("/orders")
+                .then()
+                .log()
+                .all()
+                .extract()
+                .as(OrderRealDto[].class);
+
+        for (int i = 0; i < orderRealDtoArray.length; i++ ) {
+//          System.out.println( orderRealDtoArray[i].getId() );
+            deleteOrderById( orderRealDtoArray[i].getId() );
+        }
+
+        System.out.println();
+
+//        List<OrderRealDto> list = given()
+//                .header("Content-type", "application/json")
+//                .header("Authorization", "Bearer " + token)
+//                .log()
+//                .all()
+//                .get("/orders")
+
+    }
+
+    @Test
+    public void deleteOrderByIdTest() {
+        deleteOrderById(2796);
+    }
+
+
+    public int orderCreationPrecondition() {
+
+        OrderRealDto orderRealDto = new OrderRealDto("testname", "1234567", "no");
+        Gson gson = new Gson();
+
+        int id = given()
+                .header("Content-type", "application/json")
+                .header("Authorization", "Bearer " + token)
+                .body( gson.toJson( orderRealDto ) )
+                .log()
+                .all()
+                .post("/orders")
+                .then()
+                .log()
+                .all()
+                .extract()
+                .path("id");
+
+        return id;
+
+    }
+
+    public void deleteOrderById(long id) {
+
+        given()
+                .header("Content-type", "application/json")
+                .header("Authorization", "Bearer " + token)
+                .log()
+                .all()
+                .delete("/orders/" + id)
+                .then()
+                .log()
+                .all()
+                .assertThat()
+                .statusCode(200);
+    }
+
 }
