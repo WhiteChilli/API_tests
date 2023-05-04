3315ac2b2658da747e22fb11941856de2d4f470a
WhiteChilli <locmerea@gmail.com>
2023-05-04 17:32:28 +0300

    updated for issue #6

--- src/test/java/webApp/WebAppTest.java
+++ src/test/java/webApp/WebAppTest.java
@@ -1,8 +1,10 @@
 package webApp;
-import helpers.SetupFunctions;
+
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
@@ -11,41 +13,46 @@ import static com.codeborne.selenide.Selenide.*;
 public class WebAppTest {
 
 
+    @BeforeEach
+    public void setUp() {
+        Configuration.browser = "firefox";
+        open("http://51.250.6.164:3000/signin");
+    }
+
+    @AfterEach
+    public void tearDown() {
+        closeWebDriver();
+    }
+
     @Test
     public void incorrectLogin() {
 
-        Configuration.browser = "firefox";
-        open("http://51.250.6.164:3000/signin");
-        SelenideElement usernameInput = $(By.xpath("//input[@data-name='username-input']"));
-        SelenideElement passwordInput = $(By.xpath("//input[@data-name='password-input']"));
+        LoginPage loginPage = new LoginPage();
+        loginPage.insertLogin("Hello");
+        loginPage.insertPassword("12345678");
 
-
-        usernameInput.setValue("Hello");
-        passwordInput.setValue("12345678");
-
-        $(By.xpath("//button[@data-name='signIn-button']")).click();
-        $(By.xpath("//div[@data-name='authorizationError-popup']"))
+        $(By.xpath("//button[@data-name=\"signIn-button\"]")).click();
+        $(By.xpath("//div[@data-name=\"authorizationError-popup\"]"))
                 .shouldBe(Condition.exist);
+    }
+
+    @Test
+    public void incompleteLoginDetailsButtonDisabled() {
+
+        LoginPage loginPage = new LoginPage();
+        loginPage.insertLogin("123");
+        loginPage.checkSignInDisabled();
     }
 
     @Test
     public void successfulLogin() {
 
-        SetupFunctions setupFunctions = new SetupFunctions();
+        LoginPage loginPage = new LoginPage();
+        loginPage.insertLogin("locmerea");
+        loginPage.insertPassword("hellouser123");
 
-        String username = setupFunctions.getUsername();
-        String password = setupFunctions.getPassword();
-
-        Configuration.browser = "firefox";
-        open("http://51.250.6.164:3000/signin");
-        SelenideElement usernameInput = $(By.xpath("//input[@data-name='username-input']"));
-        SelenideElement passwordInput = $(By.xpath("//input[@data-name='password-input']"));
-
-        usernameInput.setValue(username);
-        passwordInput.setValue(password);
-
-        $(By.xpath("//button[@data-name=''signIn-button']")).click();
-        $(By.xpath("//button[@data-name='createOrder-button']"))
+        $(By.xpath("//button[@data-name=\"signIn-button\"]")).click();
+        $(By.xpath("//button[@data-name=\"createOrder-button\"]"))
                 .shouldBe(Condition.exist);
     }
 }
